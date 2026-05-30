7b549cf79b21921de77d807afe5009525eb30b58
Ali 2.0 <786alidev@gmail.com>
2026-05-30 17:09:48 +0500

    Update app/core/config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
